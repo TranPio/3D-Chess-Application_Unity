cd28cac10de2a43d7500bcb4aed44080b32bdc53
Phú Trần Hoài <phubao789@gmail.com>
2024-04-07 00:48:50 +0700

    Trần Hoài Phú thêm tính năng đăng nhập, đăng kí sử dụng Firebase

---
+++ Demologinfirebase/home.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Demologinfirebase
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(home));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Broadway", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "HOME";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Broadway", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(224, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 42);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CHESS BATLLES";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(606, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Hồ sơ người dùng";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(606, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "home";
+            this.Text = "home";
+            this.Load += new System.EventHandler(this.home_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Demologinfirebase/home.cs
@@ -0,0 +1,64 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Demologinfirebase
+{
+    public partial class home : Form
+    {
+        public home()
+        {
+            InitializeComponent();
+        }
+        IFirebaseConfig config = new FirebaseConfig()
+        {
+            AuthSecret = "b2nFnPzL2nVG9NwzcjBtlInSGZXdtWFusy70UYur",
+            BasePath = "https://group14demofirebase-default-rtdb.firebaseio.com/",
+        };
+        IFirebaseClient client;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = client.Get("Information/");
+            Dictionary<string, register> result = response.ResultAs<Dictionary<string, register>>();
+            foreach (var get in result)
+            {
+                string usesrname = get.Value.Name;
+                string phone = get.Value.Phone;
+               if(phone == Form1.phone)
+                MessageBox.Show("Name: " + usesrname + "\nPhone Number: " + phone);
+                }
+        }
+
+        private void home_Load_1(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FireSharp.FirebaseClient(config);
+            }
+            catch
+            {
+                MessageBox.Show("Connection failed");
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
