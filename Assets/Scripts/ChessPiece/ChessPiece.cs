09501616fd6ecc6694ab29362dbe6dfed72dc934
Huỳnh My <huynhmy0707@gmail.com>
2024-04-25 00:28:16 +0700

    Hmee khoi tao cac quan co

---
+++ Assets/Scripts/ChessPiece/ChessPiece.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+
+public enum ChessPieceType
+{
+    None=0,
+    Pawn=1, //Tốt
+    Rook=2, //Xe
+    Knight=3,//Mã
+    Bishop=4,//Tượng
+    Queen=5,// Hậu
+    King=6 //Vua
+}
+public class ChessPiece : MonoBehaviour
+{
+    public int team;
+    public int curentX;
+    public int curentY;
+    public ChessPieceType type;
+
+    private Vector3 desiredPosition;
+    private Vector3 desiredScale;
+}
