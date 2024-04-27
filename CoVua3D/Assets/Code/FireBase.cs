945f7e704187453798a85aa9d32ad5472b229b5e
Phú Trần Hoài <phubao789@gmail.com>
2024-04-28 00:39:44 +0700

    Trần Hoài Phú thêm tính năng quên mật khẩu

--- CoVua3D/Assets/Code/FireBase.cs
+++ CoVua3D/Assets/Code/FireBase.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
+using TMPro;
+using UnityEngine.UIElements;
+using TMPro.Examples;
 
 public class FireBase : MonoBehaviour
 {
-    public GameObject loginpanel, signuppanel, homepanel, profilepanel;
-    public InputField email, password, username, emailsignup, passwordsignup;
+    public GameObject loginpanel, signuppanel, homepanel, profilepanel, forgetpasspanel, TbaoPanel;
+    public InputField email, password, username, emailsignup, passwordsignup, forgetpass;
+    public Text tbao_Text, tbao_Mess;
     public void OpenLogin()
     {
         loginpanel.SetActive(true);
         signuppanel.SetActive(false);
         homepanel.SetActive(false);
         profilepanel.SetActive(false);
+        forgetpasspanel.SetActive(false);
     }
     public void OpenSignup()
     {
@@ -20,6 +26,7 @@ public class FireBase : MonoBehaviour
         signuppanel.SetActive(true);
         homepanel.SetActive(false);
         profilepanel.SetActive(false);
+        forgetpasspanel.SetActive(false);
     }
     public void OpenHome()
     {
@@ -27,6 +34,7 @@ public class FireBase : MonoBehaviour
         signuppanel.SetActive(false);
         homepanel.SetActive(true);
         profilepanel.SetActive(false);
+        forgetpasspanel.SetActive(false);
     }
     public void OpenProfile()
     {
@@ -34,6 +42,15 @@ public class FireBase : MonoBehaviour
         signuppanel.SetActive(false);
         homepanel.SetActive(false);
         profilepanel.SetActive(true);
+        forgetpasspanel.SetActive(false);
+    }
+    public void Openforgetpass()
+    {
+        loginpanel.SetActive(false);
+        signuppanel.SetActive(false);
+        homepanel.SetActive(false);
+        profilepanel.SetActive(false);
+        forgetpasspanel.SetActive(true);
     }
     public void Exit()
     {
@@ -44,6 +61,7 @@ public class FireBase : MonoBehaviour
         if (email.text == "" || password.text == "")
         {
             Debug.Log("Vui lòng điền đầy đủ thông tin");
+            //Tbao("Lỗi", "Vui lòng điền đầy đủ thông tin");
             return;
         }
         Debug.Log("Đăng nhập thành công");
@@ -54,9 +72,29 @@ public class FireBase : MonoBehaviour
         if (emailsignup.text == "" || passwordsignup.text == "" || username.text == "")
         {
             Debug.Log("Vui lòng điền đầy đủ thông tin");
+           // Tbao("Lỗi", "Vui lòng điền đầy đủ thông tin");
             return;
         }
         Debug.Log("Đăng ký thành công");
         OpenHome();
+    }
+    public void ForgetPass()
+    {
+        if (forgetpass.text == "")
+        {
+            Debug.Log("Vui lòng điền địa chỉ email");
+            //Tbao("Lỗi", "Vui lòng điền địa chỉ email");
+            return;
+           
+        }
+        Debug.Log("Kiểm tra email của bạn");
+        OpenLogin();
+    }
+    public void Tbao(string title, string message)
+    {
+        tbao_Text.text =""+ title;
+        tbao_Mess.text = "" + message;
+        TbaoPanel.SetActive(true);
+
     }
 }
