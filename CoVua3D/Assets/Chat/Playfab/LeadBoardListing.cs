8167ac05e50382064212e8c9d08091e4f261a140
Thanhnhan <22521003@gm.uit.edu.vn>
2024-06-22 06:36:18 +0700

    Bang xep hang nguoi choi

---
+++ CoVua3D/Assets/Chat/Playfab/LeadBoardListing.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LearderboardListing : MonoBehaviour
+{
+    public Text playerNameText;
+    public Text playerScoreText;
+}
---
+++ CoVua3D/Assets/Chat/Playfab/PlayFabController.cs
@@ -0,0 +1,285 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using PlayFab.DataModels;
+using PlayFab.ProfilesModels;
+using System.Collections.Generic;
+using PlayFab.Json;
+
+public class PlayFabController : MonoBehaviour
+{
+    public static PlayFabController PFC;
+
+    private string userEmail;
+    private string userPassword;
+    private string username;
+    public GameObject loginPanel;
+    public GameObject addLoginPanel;
+    public GameObject recoverButton;
+
+    // Chỉ khai báo một lần cho leaderboardPanel và listingContainer
+    public GameObject leaderboardPanel;
+    public Transform listingContainer;
+    public GameObject listingPrefab;
+
+    private void OnEnable()
+    {
+        if (PlayFabController.PFC == null)
+        {
+            PlayFabController.PFC = this;
+        }
+        else
+        {
+            if (PlayFabController.PFC != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Start()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = "4644F"; // Vui lòng thay đổi giá trị này thành titleId của bạn từ PlayFab Game Manager
+        }
+
+        if (PlayerPrefs.HasKey("EMAIL"))
+        {
+            userEmail = PlayerPrefs.GetString("EMAIL");
+            userPassword = PlayerPrefs.GetString("PASSWORD");
+            var request = new LoginWithEmailAddressRequest
+            {
+                Email = userEmail,
+                Password = userPassword
+            };
+            PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+        }
+        else
+        {
+#if UNITY_ANDROID
+            var requestAndroid = new LoginWithAndroidDeviceIDRequest { AndroidDeviceId = ReturnMobileID(), CreateAccount = true };
+            PlayFabClientAPI.LoginWithAndroidDeviceID(requestAndroid, OnLoginAndroidSuccess, OnLoginAndroidFailure);
+#endif
+        }
+    }
+
+    #region Login
+
+    private void OnLoginAndroidSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        GetStats();
+        loginPanel.SetActive(false);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+        loginPanel.SetActive(false);
+        recoverButton.SetActive(false);
+        GetStats();
+    }
+
+    private void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+
+        PlayFabClientAPI.UpdateUserTitleDisplayName(new UpdateUserTitleDisplayNameRequest { DisplayName = username }, OnDisplayName, OnLoginAndroidFailure);
+        GetStats();
+        loginPanel.SetActive(false);
+    }
+
+    void OnDisplayName(UpdateUserTitleDisplayNameResult result)
+    {
+        Debug.Log(result.DisplayName + " is your new display name");
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        var registerRequest = new RegisterPlayFabUserRequest
+        {
+            Email = userEmail,
+            Password = userPassword,
+            Username = username
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, OnRegisterSuccess, OnRegisterFailure);
+    }
+
+    private void OnLoginAndroidFailure(PlayFabError error)
+    {
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+    private void OnRegisterFailure(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    public void GetUserEmail(string emailIn)
+    {
+        userEmail = emailIn;
+    }
+
+    public void GetUserPassword(string passwordIn)
+    {
+        userPassword = passwordIn;
+    }
+
+    public void GetUsername(string usernameIn)
+    {
+        username = usernameIn;
+    }
+
+    public void OnClickLogin()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = userEmail,
+            Password = userPassword
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    public static string ReturnMobileID()
+    {
+        string deviceID = SystemInfo.deviceUniqueIdentifier;
+        return deviceID;
+    }
+
+    public void OpenAddLogin()
+    {
+        addLoginPanel.SetActive(true);
+    }
+
+    public void OnClickAddLogin()
+    {
+        var addLoginRequest = new AddUsernamePasswordRequest
+        {
+            Email = userEmail,
+            Password = userPassword,
+            Username = username
+        };
+        PlayFabClientAPI.AddUsernamePassword(addLoginRequest, OnAddLoginSuccess, OnRegisterFailure);
+    }
+
+    private void OnAddLoginSuccess(AddUsernamePasswordResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+        GetStats();
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
+        addLoginPanel.SetActive(false);
+    }
+
+    #endregion Login
+
+    public int playerHighScore;
+
+    #region PlayerStats
+
+    public void SetStat()
+    {
+        PlayFabClientAPI.UpdatePlayerStatistics(new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate { StatisticName = "PlayerHighScore", Value = playerHighScore },
+            }
+        },
+       result => { Debug.Log("User statistics updated"); },
+       error => { Debug.LogError(error.GenerateErrorReport()); });
+    }
+
+    void GetStats()
+    {
+        PlayFabClientAPI.GetPlayerStatistics(
+            new GetPlayerStatisticsRequest(),
+            OnGetStats,
+            error => Debug.LogError(error.GenerateErrorReport())
+        );
+    }
+
+    void OnGetStats(GetPlayerStatisticsResult result)
+    {
+        Debug.Log("Received the following Statistics:");
+        foreach (var eachStat in result.Statistics)
+        {
+            Debug.Log("Statistic (" + eachStat.StatisticName + "): " + eachStat.Value);
+            switch (eachStat.StatisticName)
+            {
+                case "PlayerHighScore":
+                    playerHighScore = eachStat.Value;
+                    break;
+            }
+        }
+    }
+
+    public void StartCloudUpdatePlayerStats()
+    {
+        PlayFabClientAPI.ExecuteCloudScript(new ExecuteCloudScriptRequest()
+        {
+            FunctionName = "UpdatePlayerStats",
+            FunctionParameter = new { highScore = playerHighScore },
+            GeneratePlayStreamEvent = true,
+        }, OnCloudUpdateStats, OnErrorShared);
+    }
+
+    private static void OnCloudUpdateStats(ExecuteCloudScriptResult result)
+    {
+        Debug.Log(JsonUtility.ToJson(result.FunctionResult));
+        JsonObject jsonResult = (JsonObject)result.FunctionResult;
+        object messageValue;
+        jsonResult.TryGetValue("messageValue", out messageValue);
+        Debug.Log((string)messageValue);
+    }
+
+    private static void OnErrorShared(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    #endregion PlayerStats
+
+    #region Leaderboard
+
+    public void GetLeaderboarder()
+    {
+        var requestLeaderboard = new GetLeaderboardRequest { StartPosition = 0, StatisticName = "PlayerHighScore", MaxResultsCount = 20 };
+        PlayFabClientAPI.GetLeaderboard(requestLeaderboard, OnGetLeadboard, OnErrorLeaderboard);
+    }
+
+    void OnGetLeadboard(GetLeaderboardResult result)
+    {
+        leaderboardPanel.SetActive(true);
+        foreach (PlayerLeaderboardEntry player in result.Leaderboard)
+        {
+            GameObject tempListing = Instantiate(listingPrefab, listingContainer);
+            LearderboardListing LL = tempListing.GetComponent<LearderboardListing>();
+            LL.playerNameText.text = player.DisplayName;
+            LL.playerScoreText.text = player.StatValue.ToString();
+            Debug.Log(player.DisplayName + ": " + player.StatValue);
+        }
+    }
+
+    public void CloseLeaderboardPanel()
+    {
+        leaderboardPanel.SetActive(false);
+        for (int i = listingContainer.childCount - 1; i >= 0; i--)
+        {
+            Destroy(listingContainer.GetChild(i).gameObject);
+        }
+    }
+
+    void OnErrorLeaderboard(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    #endregion Leaderboard
+}
